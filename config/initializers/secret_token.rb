f865f64f24037497795978e13cc9b474b76e05fc
sebascastao214 <jojis-20@hotmail.com>
2013-02-28 22:05:00 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VerdeLimon::Application.config.secret_token = '1690db2d1577b0294bb1075057cd04cd8f51b2307673f70e24e58faa87fab94443f79c3c6bb08a618de353a4f7d212d84ff8aef43701447c9779eba47f0f8b80'
